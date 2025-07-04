79a98e90bdfb0fb81026a6f7d8a7d0c76fcaaaf8
soyuul <syuul3139@gmail.com>
2025-07-05 01:29:46 +0900

    chore: React 초기 설청 완료/ .env 파일 설정 완료

--- document_summary_front/src/App.js
+++ document_summary_front/src/App.js
@@ -1,8 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>여기는 메인입니다.</div> }/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
