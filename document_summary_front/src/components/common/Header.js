f1a80a5cb759765b1ba79f5fd846654de0a7b276
Soyul Jung <syuul3139@gmail.com>
2025-07-22 22:45:48 +0900

    feat: 헤더- 메인으로 바로가는 로고 삽입

--- document_summary_front/src/components/common/Header.js
+++ document_summary_front/src/components/common/Header.js
@@ -1,6 +1,9 @@
 import { height } from '@fortawesome/free-solid-svg-icons/fa0';
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import headerStyle from '../../styles/Global/Header.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
 
@@ -10,16 +13,14 @@ function Header() {
         navigate(`/`);
     }
 
-    // style
-    const headerStyle = {
-        width: "100%",
-        height: "70px",
-        backgroundColor: "red"
-    };
 
   return (
-    <div style={headerStyle}>
-      
+    <div className={headerStyle.headerBox}>
+      <div 
+      className={headerStyle.logoBox}
+      onClick={onClickMainPageHandler}>
+        <FontAwesomeIcon icon={faHouse} />
+      </div>
     </div>
   )
 }
